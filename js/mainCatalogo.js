a2e1c8222324223679f715423759ec6743ad8880
Guido Pezzotta <pezzottaguido@gmail.com>
2023-09-05 18:56:53 -0300

    modals bien hecho y agrego producto

--- js/mainCatalogo.js
+++ js/mainCatalogo.js
@@ -10,9 +10,10 @@ function Gallery(containerId, imagePaths) {
 
     this.autoRotateInterval = 5000;
 
-    this.startAutoRotate = function () {
-        this.autoRotateIntervalId = setInterval(() => this.showNextImage(), this.autoRotateInterval);
-    };
+    // No llamamos a startAutoRotate para evitar el cambio automático
+    // this.startAutoRotate = function () {
+    //     this.autoRotateIntervalId = setInterval(() => this.showNextImage(), this.autoRotateInterval);
+    // };
 
     this.stopAutoRotate = function () {
         clearInterval(this.autoRotateIntervalId);
@@ -23,7 +24,8 @@ function Gallery(containerId, imagePaths) {
     });
 
     this.showNextImage();
-    this.startAutoRotate();
+    // No llamamos a startAutoRotate para evitar el cambio automático
+    // this.startAutoRotate();
 }
 
 function openModal(imagePaths, initialIndex) {
@@ -38,12 +40,12 @@ function openModal(imagePaths, initialIndex) {
     showCurrentImage();
     modal.style.display = "block";
 
-    const autoRotateInterval = 5000;
+    /* const autoRotateInterval = 5000;
     const autoRotateIntervalId = setInterval(nextImage, autoRotateInterval);
 
     modalImage.addEventListener("click", () => {
         clearInterval(autoRotateIntervalId);
-    });
+    }); */
 
     const closeBtn = document.querySelector(".close");
     closeBtn.addEventListener("click", closeModal);
@@ -77,7 +79,25 @@ function closeModal() {
 }
 
 const gallery1 = new Gallery("imageGallery1", [
-    "./images/carteras/cartera_1_luna.jpeg",
     "./images/carteras/cartera_2_luna.jpeg",
+    "./images/carteras/cartera_1_luna.jpeg",
+    "./images/carteras/cartera_3_luna.jpeg",
     // Agrega más rutas de imágenes según sea necesario
-]);
+])
+
+const gallery2 = new Gallery("imageGallery2", [
+    "./images/carteras/cartera_celeste_2.jpeg",
+    "./images/carteras/cartera_celeste_1.jpeg",
+    "./images/carteras/cartera_celeste_3.jpeg",
+    "./images/carteras/cartera_celeste_4.jpeg",
+    "./images/carteras/cartera_celeste_5.jpeg",
+    // Agrega más rutas de imágenes según sea necesario
+])
+
+const gallery3 = new Gallery("imageGallery3", [
+    "./images/carteras/cartera_lila_1.jpeg",
+    "./images/carteras/cartera_lila_2.jpeg",
+    "./images/carteras/cartera_lila_3.jpeg",
+    "./images/carteras/cartera_lila_4.jpeg",
+    // Agrega más rutas de imágenes según sea necesario
+])
